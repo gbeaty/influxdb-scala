5494f4579ae7cb31f6597a26d604e878b4e050d9
Grant Beaty <gbeaty@gmail.com>
2015-06-13 18:53:22 -0400

    2.11.6 build.

--- project/build.scala
+++ project/build.scala
@@ -10,8 +10,7 @@ object InfluxBuild extends Build {
       name := "influxdb-scala-core",
       organization := org,
       version := "0.6-SNAPSHOT",
-      scalaVersion := "2.10.3",
-      crossScalaVersions := Seq("2.10.3","2.11.0-M7"),
+      scalaVersion := "2.11.6",
       libraryDependencies ++= Seq(
         "org.slf4j" % "slf4j-api" % "1.7.7"
 
@@ -26,41 +25,13 @@ object InfluxBuild extends Build {
       name := "influxdb-scala-standalone",
       organization := org,
       version := "0.6-SNAPSHOT",
-      scalaVersion := "2.10.3",
-      crossScalaVersions := Seq("2.10.3","2.11.0-M7"),
+      scalaVersion := "2.11.6",
       mainClass := Some("TestApp"),
       libraryDependencies ++= Seq(
-    		  "org.json4s" % "json4s-native_2.10" % "3.2.6",
+    		  "org.json4s" %% "json4s-native" % "3.2.11",
     		  "com.ning" % "async-http-client" % "1.7.19",
-          "org.scalatest" % "scalatest_2.10" % "2.0" % "test",
+          "org.scalatest" %% "scalatest" % "2.2.4" % "test",
           "org.mockito" % "mockito-core" % "1.9.5" % "test"
       )
   )
-
-  //============================================================================
-  // Comment out the following two sub projects if you have issues building this
-  //============================================================================
-
-  /* I give up... Why doesn't this work?????????
-
-  // this project contains the Map[String,Any] <-> case class mapping macro and only builds with scala 2.11
-  lazy val macro = Project("macro", file("macro")) settings(
-      name := "influxdb-scala-macro",
-      organization := org,
-      version := "0.1",
-      scalaVersion := "2.11.0-M7",
-      libraryDependencies <+= scalaVersion("org.scala-lang" % "scala-compiler" % _),
-      libraryDependencies <+= scalaVersion("org.scala-lang" % "scala-reflect" % _)
-  )
-
-
-  // this project contains the typed API which depends on both the core untyped API as well as the macros
-  // which unfortunately only compile with the 2.11 milestone of scala
-  lazy val experimental = Project("experimental", file("experimental")) dependsOn(core, macro, standalone) settings(
-      name := "influxdb-scala-experimental",
-      organization := org,
-      version := "0.1",
-      scalaVersion := "2.11.0-M7"
-  )
-  */
 }
